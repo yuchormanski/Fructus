43be7af9267a13febd35b4d0a868be2e715200bc
yuchormanski <yuchormanski@gmail.com>
2024-04-25 17:55:56 +0300

    work on card view

---
+++ Fructus/View/FruitCardView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Nikolay  Yuchormanski on 25.04.24.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitCardView()
+}
