5e385c8be9c4d4253998d698549419e70614bc15
yuchormanski <yuchormanski@gmail.com>
2024-04-25 12:31:26 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Nikolay  Yuchormanski on 25.04.24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
